3b830b6964e0f3b6a034519e713eaea2e0393318
JSwambo <jake.t.swambo@hotmail.co.uk>
2020-02-27 12:29:46 +0000

    Added file to run the protocol and cleanly handle running bitcoind instances on regtest.

---
+++ run_protocol.py
@@ -0,0 +1,29 @@
+import os
+import time
+from address_gen import *
+
+# # Set up clean regtest environment
+
+# Kill any running instances of bitcoind
+os.system('killall bitcoind')
+
+# Delete old regtest data
+try:
+    os.rmdir('~/.bitcoin/regtest/')
+except(FileNotFoundError):
+    print("Delete failed. No previous regtest data found.")
+
+# Start new instance of bitcoind
+os.system('~/bitcoin/src/bitcoind -daemon')
+time.sleep(5)
+
+# # Begin vault-custody protocol
+
+# deposit to vault
+os.system("python3 deposit_to_vault.py")
+
+# prepare vault transaction
+os.system("python3 prepare_vault_tx.py")
+
+
+
