f90b58cd4f3169d058a758eb1998f4c502d3b680
JSwambo <jake.t.swambo@hotmail.co.uk>
2020-02-27 13:59:25 +0000

    Added script to generate transaction spending vault P2SH output with active wallet.

--- run_protocol.py
+++ run_protocol.py
@@ -6,6 +6,7 @@ from address_gen import *
 
 # Kill any running instances of bitcoind
 os.system('killall bitcoind')
+time.sleep(2)
 
 # Delete old regtest data
 try:
@@ -15,7 +16,7 @@ except(FileNotFoundError):
 
 # Start new instance of bitcoind
 os.system('~/bitcoin/src/bitcoind -daemon')
-time.sleep(5)
+time.sleep(4)
 
 # # Begin vault-custody protocol
 
@@ -25,5 +26,5 @@ os.system("python3 deposit_to_vault.py")
 # prepare vault transaction
 os.system("python3 prepare_vault_tx.py")
 
-
-
+# unvault_to_active_wallet.py
+os.system("python3 unvault_to_active_wallet.py")
---
+++ unvault_to_active_wallet.py
@@ -0,0 +1,62 @@
+from address_gen import *
+import pickle
+import pprint
+from bitcoin.core import b2lx, lx, COIN, COutPoint, CTxOut, CTxIn, CTxInWitness, CTxWitness, CScriptWitness, CMutableTransaction, Hash160, CMutableTxIn
+from bitcoin.core.scripteval import VerifyScript, SCRIPT_VERIFY_P2SH
+
+# # Connect to bitcoind through RPC
+SelectParams("regtest")
+connection = Proxy()
+
+# # Specify which utxo to spend from
+with open('signed_vault_tx.pkl', 'rb') as f:
+    signed_deposit_tx = pickle.load(f)
+
+txid = signed_deposit_tx['txid']
+vout = signed_deposit_tx['vout'][0]['n']
+amount = int(float(signed_deposit_tx['vout'][0]['value']) * COIN)
+
+
+# # # Calculate an amount for the upcoming new UTXO. Set a high fee to bypass
+# # # bitcoind minfee setting.
+amount_less_fee = int(amount - (0.01 * COIN))
+
+# # Create the unsigned unvault spend transaction.
+txin = CMutableTxIn(COutPoint(lx(txid), vout))
+txout = CTxOut(amount_less_fee, depositor_redeemScript)
+tx = CMutableTransaction([txin], [txout])
+
+# # # Specify which transaction input is going to be signed for.
+txin_index = 0
+
+# Calculate the signature hash for that transaction. Note how the script we use
+# is the redeemScript, not the scriptPubKey. That's because when the CHECKSIG
+# operation happens EvalScript() will be evaluating the redeemScript, so the
+# corresponding SignatureHash() function will use that same script when it
+# replaces the scriptSig in the transaction being hashed with the script being
+# executed.
+sighash = SignatureHash(vault_out_redeemScript, tx, txin_index, SIGHASH_ALL)
+
+# Now sign it. We have to append the type of signature we want to the end, in
+# this case the usual SIGHASH_ALL.
+sig1 = AW_privkeys[0].sign(sighash) + bytes([SIGHASH_ALL])
+sig2 = AW_privkeys[1].sign(sighash) + bytes([SIGHASH_ALL])
+
+# Set the scriptSig of our transaction input appropriately.
+txin.scriptSig = CScript([OP_0, sig1, sig2, vault_out_redeemScript])
+
+# Verify the signature worked. This calls EvalScript() and actually executes
+# the opcodes in the scripts to see if everything worked out. If it doesn't an
+# exception will be raised.
+VerifyScript(txin.scriptSig, vault_out_redeemScript.to_p2sh_scriptPubKey(), tx, 0, (SCRIPT_VERIFY_P2SH,))
+
+# #  Broadcast the transaction to the regtest network.
+# spend_txid = connection.sendrawtransaction(tx)
+
+file_name = "unvault_to_active_wallet.pkl"
+
+with open(file_name, 'wb') as f:
+    pickle.dump(connection._call('decoderawtransaction', b2x(tx.serialize())), f)
+
+print(f"Spent transaction {b2lx(spend_txid)} and saved to file {file_name}. \n")
+pprint.pprint(connection._call('decoderawtransaction', b2x(tx.serialize())))
